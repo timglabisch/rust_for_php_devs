4810314fe678a998a0fd65b0e4f493eeab193e9b
timglabisch <tim@ainfach.de>
2016-07-02 17:52:02 +0200

    yay

--- js/reveal.js
+++ js/reveal.js
@@ -39,8 +39,8 @@
 
 			// The "normal" size of the presentation, aspect ratio will be preserved
 			// when the presentation is scaled to fit different resolutions
-			width: 1920,
-			height: 1080,
+			width: 960,
+			height: 700,
 
 			// Factor of the display size that should remain empty around the content
 			margin: 0.01,
@@ -1995,10 +1995,10 @@
 	 */
 	function enterFullscreen() {
 
-		var element = document.body;
+		var element = document.documentElement;
 
 		// Check which implementation is available
-		var requestMethod = element.requestFullScreen ||
+		var requestMethod = element.requestFullscreen ||
 							element.webkitRequestFullscreen ||
 							element.webkitRequestFullScreen ||
 							element.mozRequestFullScreen ||
